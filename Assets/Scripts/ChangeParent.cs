166dd71ca5563ccae1df142f5403e32692e6f43c
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-11-23 12:53:43 +0000

    Switching parents now disables the interactive

--- Assets/Scripts/ChangeParent.cs
+++ Assets/Scripts/ChangeParent.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class ChangeParent : SpecialInteractions
 {
-    [SerializeField] GameObject pieceToChange;
+    [SerializeField] private GameObject     pieceToChange;
+    private Interactive                     pieceToChangeInteractive;
+
+    private void Awake()
+    {
+        pieceToChangeInteractive = pieceToChange.GetComponent<Interactive>();
+    }
 
     public override void SpecialInteract(Interactive interactive)
     {
         pieceToChange.SetActive(true);
         pieceToChange.transform.SetParent(gameObject.transform, false);
+        pieceToChangeInteractive.isOn = false;
     }
 }
