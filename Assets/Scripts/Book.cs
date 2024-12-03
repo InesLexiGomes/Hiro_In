ed56e00606c35eb0b2b32a9cccddad88db6c3b99
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-12-03 18:23:43 +0000

    Made it so book pages can have a back side

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Book : MonoBehaviour
 {
     [SerializeField] float pageSpeed = 0.5f;
-    [SerializeField] List<Transform> pages;
+    [SerializeField] List<Transform> pagesFront;
+    [SerializeField] List<Transform> pagesBack;
     int index = -1;
     bool rotate = false;
     [SerializeField] GameObject backButton;
@@ -18,11 +19,12 @@ public class Book : MonoBehaviour
 
     public void InitialState()
     {
-        for (int i=0; i<pages.Count; i++)
+        for (int i=0; i<pagesFront.Count; i++)
         {
-            pages[i].transform.rotation=Quaternion.identity;
+            pagesFront[i].transform.rotation=Quaternion.identity;
+            pagesBack[i].transform.rotation=Quaternion.identity;
         }
-        pages[0].SetAsLastSibling();
+        pagesFront[0].SetAsLastSibling();
         backButton.SetActive(false);
 
     }
@@ -33,7 +35,7 @@ public class Book : MonoBehaviour
         index++;
         float angle = 180; //in order to rotate the page forward, you need to set the rotation by 180 degrees around the y axis
         ForwardButtonActions();
-        pages[index].SetAsLastSibling();
+        pagesBack[index].SetAsLastSibling();
         StartCoroutine(Rotate(angle, true));
 
     }
@@ -44,7 +46,7 @@ public class Book : MonoBehaviour
         {
             backButton.SetActive(true); //every time we turn the page forward, the back button should be activated
         }
-        if (index == pages.Count - 1)
+        if (index == pagesFront.Count - 1)
         {
             forwardButton.SetActive(false); //if the page is last then we turn off the forward button
         }
@@ -54,7 +56,7 @@ public class Book : MonoBehaviour
     {
         if (rotate == true) { return; }
         float angle = 0; //in order to rotate the page back, you need to set the rotation to 0 degrees around the y axis
-        pages[index].SetAsLastSibling();
+        pagesFront[index].SetAsLastSibling();
         BackButtonActions();
         StartCoroutine(Rotate(angle, false));
     }
@@ -80,8 +82,9 @@ public class Book : MonoBehaviour
             rotate = true;
             Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
             value += Time.deltaTime * pageSpeed;
-            pages[index].rotation = Quaternion.Slerp(pages[index].rotation, targetRotation, value); //smoothly turn the page
-            float angle1 = Quaternion.Angle(pages[index].rotation, targetRotation); //calculate the angle between the given angle of rotation and the current angle of rotation
+            pagesFront[index].rotation = Quaternion.Slerp(pagesFront[index].rotation, targetRotation, value); //smoothly turn the page
+            pagesBack[index].rotation = Quaternion.Slerp(pagesBack[index].rotation, targetRotation, value); //smoothly turn the page
+            float angle1 = Quaternion.Angle(pagesFront[index].rotation, targetRotation); //calculate the angle between the given angle of rotation and the current angle of rotation
             if (angle1 < 0.1f)
             {
                 if (forward == false)
