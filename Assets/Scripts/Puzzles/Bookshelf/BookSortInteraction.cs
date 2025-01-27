faed5cb15791bc7b99a807e3579ac502eb2cad2f
Inês Gomes <inesgomes.pessoal05@gmail.com>
2025-01-27 17:18:04 +0000

    Added sound for books being pulled

--- Assets/Scripts/Puzzles/Bookshelf/BookSortInteraction.cs
+++ Assets/Scripts/Puzzles/Bookshelf/BookSortInteraction.cs
@@ -6,16 +6,19 @@ public class BookSortInteraction : SpecialInteractions
     [SerializeField] private int index;
     [SerializeField] private Animator animator;
     [SerializeField] private Interactive bookInteractive;
+    [SerializeField] private AudioSource source;
 
     public override void SpecialInteract(Interactive interactive)
     {
         animator.ResetTrigger("Reset");
         sortingPuzzle.AddIndex(index);
         animator.Play("BookOut");
+        source.Play();
     }
 
     public void ResetBook()
     {
         animator.SetTrigger("Reset");
+        source.Play();
     }
 }
